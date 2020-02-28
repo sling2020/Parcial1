9a2f50e7d15c040c7f936e622127e27fd69db9b4
sling2020 <61530734+sling2020@users.noreply.github.com>
2020-02-27 19:17:14 -0500

    Add files via upload

--- potencia.py
+++ potencia.py
@@ -1,24 +1,25 @@
 import sys
-class Lenguaje():
-                            
-    def __init__(self,lenguaje):        
-        self.lenguaje = lenguaje
-        self.palabras = 1
+if __name__ == '__main__':    
+    class Lenguaje():
+                                
+        def __init__(self,lenguaje):        
+            self.lenguaje = lenguaje
+            self.palabras = 1
+            
+            for i in range(len(self.lenguaje)):
+                if(self.lenguaje[i] == ","):
+                    self.palabras += 1
+            print("llega hsta aquí")
         
-        for i in range(len(self.lenguaje)):
-            if(self.lenguaje[i] == ","):
-                self.palabras += 1
-        print("llega hsta aquí")
-    
-    def palabraDesde(pos):
-        tamano = len(self.lenguaje)
-        for i in range(len(self.lenguaje[pos:tamano])):
-            if(self.lenguaje[i] == "," or self.lenguaje[i] == "}"):
-                palabra = self.lenguaje[pos:i]
-                break
-                return palabra
+        def palabraDesde(pos):
+            tamano = len(self.lenguaje)
+            for i in range(len(self.lenguaje[pos:tamano])):
+                if(self.lenguaje[i] == "," or self.lenguaje[i] == "}"):
+                    palabra = self.lenguaje[pos:i]
+                    break
+                    return palabra
 
+lenguaje = sys.argv[1]                
+objeto = Lenguaje(lenguaje[2:len(lenguaje)])
 
-objeto = Lenguaje("{bo,be,vi,ni}")
-print(objeto.palabras)
 
